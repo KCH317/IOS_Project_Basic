cb35ff165ac26338bc55254a6b2a91ab9f3cc393
KCH317 <rnjscksgh94@gmail.com>
2022-02-16 22:28:23 +0900

    화면간 데이터 전달하기
    sugue, code 방식에서 데이터 전달방법

--- ScreenTransitionExample/ScreenTransitionExample/CodePresentViewController.swift
+++ ScreenTransitionExample/ScreenTransitionExample/CodePresentViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
+protocol SendDataDelegate: AnyObject{
+    func sendData(name: String)
+}
 class CodePresentViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    weak var delegate: SendDataDelegate? // weak를 붙여야 데이터 누수를 막음
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func tapBackButton(_ sender: UIButton) {
+        self.delegate?.sendData(name: "Kwon")
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
