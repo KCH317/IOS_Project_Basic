e0862aede3067c0c93dbf42648f822e95b1e5aa1
KCH317 <rnjscksgh94@gmail.com>
2022-02-24 23:45:12 +0900

    5-일기앱
    - 일기작성 화면 기능구현

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 권찬호 on 2022/02/24.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
