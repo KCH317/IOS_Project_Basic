4d81cd8b1bddad61f3d6609187fa8638059b602d
KCH317 <rnjscksgh94@gmail.com>
2022-03-05 23:55:44 +0900

    8-코로나앱
    - 코로나 데이터 받아오기
    - 실제 데이터를 화면에 표시하기
    - 파이차트를 이용한 UI개선

--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -16,9 +18,116 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: {
+            [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+//                debugPrint("success \(result)")
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configureChatView(covidOverviewList: covidOverviewList)
+                
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        })
+    }
+    
+    // 파이차트에 표시
+    func configureChatView(covidOverviewList: [CovidOverview]){
+        let entries = covidOverviewList.compactMap{
+            [weak self] overview -> PieChartDataEntry? in
+            guard let self = self else { return nil }
+            return PieChartDataEntry(
+                value: self.removeFormatString(string: overview.newCase),
+                label: overview.countryName,
+                data: overview)
+        }
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        
+        // 파이차트 색상
+        dataSet.colors = ChartColorTemplates.vordiplom() +
+            ChartColorTemplates.joyful() +
+            ChartColorTemplates.liberty() +
+            ChartColorTemplates.pastel() +
+            ChartColorTemplates.material()
+        
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+        self.pieChartView.spin(duration: 0.3, fromAngle: self.pieChartView.rotationAngle, toAngle: self.pieChartView.rotationAngle + 80)
+    }
+    
+    // string -> double
+    func removeFormatString(string: String) -> Double{
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)? .doubleValue ?? 0
     }
 
+    // 파이차트에 표시
+    func makeCovidOverviewList(
+        cityCovidOverview: CityCovidOverview
+    ) -> [CovidOverview]{
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.daegu,
+            cityCovidOverview.incheon,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.sejong,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.jeju
+            
+        ]
+    }
+    
+    // 확진자 표시
+    func configureStackView(koreaCovidOverview: CovidOverview){
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
+        
+    }
 
+    // 코로나 데이터 받아오기
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result <CityCovidOverview, Error>) -> Void
+        
+    ){
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey" : "mHTRlq24fVrOzA56xb3cZyQjkBFdwe9UE"
+        ]
+        
+        // 요청
+        AF.request(url, method: .get, parameters: param).responseData(completionHandler: { // 응답
+            response in
+            switch response.result {
+            case let .success(data):
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(CityCovidOverview.self, from: data)
+                    completionHandler(.success(result))
+                } catch {
+                    completionHandler(.failure(error))
+                }
+            case let .failure(error):
+                completionHandler(.failure(error))
+            }
+        })
+        
+    }
 }
 
